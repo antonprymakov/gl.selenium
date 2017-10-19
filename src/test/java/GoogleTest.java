bebd098bf65c77ca670ad74c1208c3b87d388521
Anton Prymakov <anton.prymakov@synapse.com>
2017-10-19 14:38:27 +0300

    Initial commit

---
+++ src/test/java/GoogleTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Created by anton.prymakov on 10/19/2017.
+ */
+public class GoogleTest {
+
+    @Test
+    public void googleTest(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+        driver.findElement(By.id("lst-ib")).sendKeys("test");
+
+        //driver.quit();
+    }
+
+}
